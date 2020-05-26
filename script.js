d614e30fc811574ef37d62d1b9a730f5c166f2fa
Darylabrador <abrador.daryl@gmail.com>
2020-05-26 16:26:45 +0400

    rajout d'un fonction de sécurité

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ let req = new XMLHttpRequest();
 let url = "https://spreadsheets.google.com/feeds/cells/1ffY6Q5YdF53VcEMcFxnvGcie3bbKx8WzMKr0R9-RWi8/1/public/full?alt=json" ;
 let etudiant = document.querySelector('#simplon');
 
+
+// Fonction de sécurité 
+
+function htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
 function getDataFromGoogleSheetAPI(){
 
     // On récupère le JSON
@@ -20,17 +27,7 @@ function getDataFromGoogleSheetAPI(){
             // Récupérer nos informations du tableau
             let infos = reponse.feed.entry;
 
-            // Pour avoir le nombre de colonne pour le découpage
-            let colonne = [];
-
-            for(let i = 0; i < infos.length; i++){
-                colonne.push(infos[i].gs$cell.col);
-            }
-
-            // On récupère l'ensemble des valeurs, puis on le rend unique et on fini par regarder la valeur max
-            let colonneUnique = [...new Set(colonne)];
-            let maxColonne = Math.max(...colonneUnique);
-
+            // les tableaux pour les entetes et les data
             let tableauData = [];
             let tableauEntete = [];
 
@@ -49,25 +46,33 @@ function getDataFromGoogleSheetAPI(){
 
             // Pour la création du header du tableau
             for (let k = 0; k < tableauEntete.length; k++) {
+
+                // variable avec condition ternaire pour verifier si la cellule n'est pas vide
+                var headerInsert = tableauEntete[k].content.$t.toUpperCase(); 
+
                 let td = document.createElement('td');
-                td.innerHTML += tableauEntete[k].content.$t.toUpperCase();
+                td.innerHTML += htmlEntities(headerInsert);
                 head.appendChild(td);
             }
 
             // Pour la création du body du tableau
             for (let j = 0; j < tableauData.length; j++){
 
+                // variable avec condition ternaire pour verifier si la cellule n'est pas vide
+                var dataInsert = tableauData[j].content.$t; 
+
                 // On crée la ligne et à chaque fois que la colonne est 1, on crée
                 if (tableauData[j].gs$cell.col == 1){
 
                     // Pour la valeur de la première colonne
                     var tr = document.createElement('tr');
-                    tr.innerHTML += tableauData[j].content.$t;      
+                    tr.innerHTML += htmlEntities(dataInsert); 
+
                 }else{
 
                     // Pour la valeur des autres colonnes puis on fini par l'ajouter au html
                     let td = document.createElement('td');
-                    td.innerHTML = tableauData[j].content.$t;
+                    td.innerHTML = htmlEntities(dataInsert);
                     tr.appendChild(td);
                     body.appendChild(tr);
                 }
